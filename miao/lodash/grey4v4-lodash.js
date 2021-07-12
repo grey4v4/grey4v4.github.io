dae35192b02538eb62f4ac9592e08a3a4104833c
grey <grey4v4@163.com>
2021-07-12 16:00:33 +0800

    fitst

--- miao/lodash/grey4v4-lodash.js
+++ miao/lodash/grey4v4-lodash.js
@@ -1,9 +1,20 @@
 var grey4v4 = function () {
-  function chunk() {
-
+  function chunk(array, number) {
+    var result = []
+    var count = 0
+    for (var i = 0; i < array.length; i++) {
+      result = result.push(array[i])
+      count++
+      if (count == number) {
+        count = 0
+      }
+    }
   }
+
+  
+
+  
   return {
-    chunk: chunk,
-    compact: compact,
+    chunk: chunk
   }
 }()
